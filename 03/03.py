6de33433e16ee805383d2d342aad2b76dbe42b11
suriiibb <857735978@qq.com>
2019-09-30 19:21:13 +0800

    hw3

--- 03/03.py
+++ 03/03.py
@@ -1,34 +1,35 @@
-def choose():
-    print ('Please choose among the programs:p01,p02,p03')
-    print ('p01 can greet you as\'Hello, world!\' ')
-    print ('p02 can greet you as \'Hello, your name\' ')
-    print ('p03 can convert your Celsius input into Farenheit')
-    opt = input ('Please choose your desired function')
-
 #below is the code for program p01
 def p01():
     func1 = 'Hello, world'
-    return
+    return func1
 
 #below is the code for program p02
-def p02():
+def p02_1():
     name = input ('Please enter your name')
-    func2 = 'Hello' + name
-    return
+    func2 = 'Hello,' + name
+    return func2
 
 #below is the code for program p03
-def p03():
+def p02_2():
     C = float(input('Enter the temperature in Celsius'))
     func3 = C * 1.8 + 32
-    return
+    return func3
 
-if opt = 'p01':
-    print (func1)
-elif opt = 'p02':
-    print (func2)
-elif opt = 'p03':
-    print (func3)
-else:
-    print ('Invalid input')
+while 1>0:
+    def choose():
+        print ('Please choose among the programs:p01,p021,p022')
+        print ('p01 can greet you as\'Hello, world!\' ')
+        print ('p02_1 can greet you as \'Hello, your name\' ')
+        print ('p02_2 can convert your Celsius input into Farenheit')
+        opt = input ('Please choose your desired function')
+        if opt == 'p01':
+            print (p01())
+        elif opt == 'p02_1':
+            print (p02_1())
+        elif opt == 'p02_2':
+            print (p02_2())
+        else:
+            print ('Invalid input')
 
-choose()
+
+    choose()
